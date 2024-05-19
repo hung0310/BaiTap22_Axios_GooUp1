3980f1a8903720976b8fc249549352ced49b1497
Cnotdher_10 <107135919+hung0310@users.noreply.github.com>
2024-05-19 14:44:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+// --------GET-------
 axios.get('https://dummyjson.com/products')
+    .then(res => {
+        console.log(res.data);
+    })
+
+// -------POST-------
+var newPoduct = {
+    title: 'Hello',
+    description: 'Hello',
+    price: 10,
+    discountPercentage: 10,
+};
+
+axios.post('https://dummyjson.com/products/add', newPoduct)
+    .then(res => {
+        console.log(res.data);
+    })
+
+// --------PUT-------
+
+var upProduct = {
+    title: 'Bye',
+};
+
+axios.put('https://dummyjson.com/products/1', upProduct)
+    .then(res => {
+        console.log(res.data);
+    })
+
+// ------DELETE------
+axios.delete('https://dummyjson.com/products/1')
+    .then(res => {
+        console.log(res.data);
+    })axios.get('https://dummyjson.com/products')
     .then(res => {
         console.log(res.data);
     }) 
